822041bb4b9e88b022f0291733391489545bfac9
Keith Cirkel <github@keithcirkel.co.uk>
2012-01-09 08:43:07 +0000

    First iteration of website. Includes warning banner at the top of the site saying it is under construction. Changes soon!

---
+++ _posts/2012-02-22-tempus-now-released.md
@@ -0,0 +1,7 @@
+---
+layout: blog_post
+title: Tempus is now released
+---
+<p>
+    It gives me great pleasure to announce that the Tempus javascript library has been released!
+</p>
---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "_includes/css"
+sass_dir = "_includes/css"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+output_style = (environment == :production) ? :compressed : :expanded
