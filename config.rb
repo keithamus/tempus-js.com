e4b6a24e192c1ae39b3527d0b49c14cf33a45c39
Keith Cirkel <github@keithcirkel.co.uk>
2012-03-28 18:37:30 +0100

    Always compress CSS to prevent expanded CSS from being deployed

--- config.rb
+++ config.rb
@@ -4,4 +4,4 @@ sass_dir = "_includes/css"
 images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
-output_style = (environment == :production) ? :compressed : :expanded
+output_style = :compressed
